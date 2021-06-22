2f2d2bb6c3cae359dab616ac537c7751b9dc6563
Jonas Gao <gaopinsong@live.cn>
2021-06-22 14:46:41 +0800

    feat: add build/get/latest cmd

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/urfave/cli/v2"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -24,6 +26,89 @@ func main() {
 					jobs, err := client.GetAllJobs(ctx)
 					for i := range jobs {
 						fmt.Printf("%s\n", jobs[i].GetName())
+					}
+					return nil
+				},
+			},
+			{
+				Name:    "build",
+				Aliases: []string{"b"},
+				Usage:   "Build the job",
+				Action: func(c *cli.Context) error {
+					if c.NArg() <= 0 {
+						return errors.New("please provide a JOB name")
+					}
+					jobName := c.Args().Get(0)
+					client, ctx, err := getClient()
+					if err != nil {
+						panic(err)
+					}
+					job, err := client.BuildJob(ctx, jobName, nil)
+					fmt.Printf("Build number: %d", job)
+					return err
+				},
+			},
+			{
+				Name:  "get",
+				Usage: "Get the job builds",
+				Action: func(c *cli.Context) error {
+					client, ctx, err := getClient()
+					if err != nil {
+						return err
+					}
+					nArg := c.NArg()
+					if nArg <= 0 {
+						return errors.New("please provide a JOB name")
+					}
+					jobName := c.Args().Get(0)
+					if nArg == 1 {
+						ids, err := client.GetAllBuildIds(ctx, jobName)
+						if err != nil {
+							return err
+						}
+						for _, id := range ids {
+							fmt.Printf("%d: %s\n", id.Number, id.URL)
+						}
+					} else {
+						buildNumber := c.Args().Get(1)
+						num, err := strconv.ParseInt(buildNumber, 10, 64)
+						if err != nil {
+							return err
+						}
+						build, err := client.GetBuild(ctx, jobName, num)
+						if err != nil {
+							return err
+						}
+						running := build.IsRunning(ctx)
+						result := build.GetResult()
+						fmt.Printf("Running: %t; Result: %s", running, result)
+					}
+					return nil
+				},
+			},
+			{
+				Name:  "latest",
+				Usage: "Get the job latest build",
+				Action: func(c *cli.Context) error {
+					client, ctx, err := getClient()
+					if err != nil {
+						return err
+					}
+					nArg := c.NArg()
+					if nArg <= 0 {
+						return errors.New("please provide a JOB name")
+					}
+					jobName := c.Args().Get(0)
+					ids, err := client.GetAllBuildIds(ctx, jobName)
+					if len(ids) > 0 {
+						id := ids[0]
+						build, err := client.GetBuild(ctx, jobName, id.Number)
+						if err != nil {
+							return err
+						}
+						running := build.IsRunning(ctx)
+						result := build.GetResult()
+						fmt.Printf("Running: %t; Result: %s", running, result)
 					}
 					return nil
 				},
